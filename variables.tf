07b51f0013abd93fb72a093ab107c59715a31f26
kraken <javascriptiqbal@gmail.com>
2026-01-13 22:05:26 +0600

    ec2 spawn

--- variables.tf
+++ variables.tf
@@ -1,22 +1,20 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "eu-central-1"
+  default     = "eu-west-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.large"
 }
 
 variable "instance_count" {
   description = "Number of EC2 instances to provision"
   type        = number
-  default     = 5
+  default     = 1
 }
-
-
 
 variable "volume_size" {
   description = "Size of the root volume in GB"
